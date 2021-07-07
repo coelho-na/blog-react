1f5ecc2af4fd2acc1d9e9eef769bf144a2637df8
Nadine Coelho <nadinecoelho9@gmail.com>
2021-07-07 19:26:40 -0300

    change name 'blog' to 'post'

--- src/PostList.js
+++ src/PostList.js
@@ -1,11 +1,15 @@
+import { Link } from "react-router-dom";
+
 const PostList = ({ posts, title }) => {
   return (
     <div className="posts">
       <h2>{title}</h2>
       {posts.map((post) => (
         <div className="posts__preview" key={post.id}>
-          <h2 className="posts__title">{post.title}</h2>
-          <p>Written by {post.author}</p>
+          <Link className="posts__link" to={`/blogs/${post.id}`}>
+            <h2 className="posts__title">{post.title}</h2>
+            <p>Written by {post.author}</p>
+          </Link>
         </div>
       ))}
     </div>
